0f8e60907e62ea363f7b1cd6115d160adc985098
Srikanya2023 <122676448+Srikanya2023@users.noreply.github.com>
2023-01-14 18:30:16 +0530

    central commit
    central

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello
 {
 public static void main (string args[])
 {
-system.out.println ("hello happy nwe year 2023 from central rep")
+system.out.println ("hello happy new year 2023 from central rep")
 }
 }
